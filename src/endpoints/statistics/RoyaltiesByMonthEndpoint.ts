81327169219c3e23313fb94421909266418d9bba
loudar <Alexander Fritsch>
2025-01-12 22:45:52 +0100

    3 years for single stats, 1y for all

--- src/endpoints/statistics/RoyaltiesByMonthEndpoint.ts
+++ src/endpoints/statistics/RoyaltiesByMonthEndpoint.ts
@@ -20,14 +20,14 @@ export class RoyaltiesByMonthEndpoint extends AuthenticatedGetEndpoint {
         const upc = req.query.upc as string;
         const isrc = req.query.isrc as string;
         if (upc) {
-            months = await this.db.getRoyaltiesByMonthForUPC(upc, 15);
+            months = await this.db.getRoyaltiesByMonthForUPC(upc, 36);
         } else if (isrc) {
-            months = await this.db.getRoyaltiesByMonthForISRC(isrc, 15);
+            months = await this.db.getRoyaltiesByMonthForISRC(isrc, 36);
         } else {
             const artists = await this.db.getUserArtists(user.id);
             const artistNames = artists.map(a => a.name);
 
-            months = await this.db.getRoyaltiesByMonth(artistNames, 15);
+            months = await this.db.getRoyaltiesByMonth(artistNames, 12);
         }
         if (!months || months.length === 0) {
             return res.send([]);
