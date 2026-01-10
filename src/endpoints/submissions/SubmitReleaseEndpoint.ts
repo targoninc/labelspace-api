302d9b952b82f42833ee6ab10365f72f74ab88d8
alex <alex@targoninc.com>
2026-01-10 11:49:07 +0100

    send email to submitter

--- src/endpoints/submissions/SubmitReleaseEndpoint.ts
+++ src/endpoints/submissions/SubmitReleaseEndpoint.ts
@@ -25,7 +25,22 @@ export class SubmitReleaseEndpoint extends PostEndpoint {
             }
         });
 
-        const mailContent = MailBuilder.default("https://artists.trirecords.eu/images/LOGO128.png")
+        const forSubmitterMail = MailBuilder.default("https://artists.trirecords.eu/images/LOGO128.png")
+            .subject("Your Tri Records submission")
+            .heading("We've received your Tri Records submission!")
+            .paragraph(`You just submitted a release as artist ${request.artistName} with a desired release date of ${request.desiredReleaseDate}`)
+            .paragraph("You can expect a response from us within the next 3-5 days. Sometimes it can take longer. Thank you for your patience!")
+            .card([
+                paragraph(request.message)
+            ])
+            .card([
+                heading("Link to your submission", 2),
+                link(request.link),
+            ])
+            .signature("the Tri Records Team", "Targon Industries UG")
+            .get();
+
+        const forTriMail = MailBuilder.default("https://artists.trirecords.eu/images/LOGO128.png")
             .subject("Tri Records submission")
             .heading("Tri Records submission")
             .paragraph(`New release submission from ${request.artistName} for ${request.desiredReleaseDate}`)
@@ -41,8 +56,10 @@ export class SubmitReleaseEndpoint extends PostEndpoint {
             .get();
 
         for (const mail of mails) {
-            Mail.sendDefault(mail, mailContent);
+            Mail.sendDefault(mail, forTriMail);
         }
+
+        Mail.sendDefault(request.email, forSubmitterMail);
 
         return res.send("OK");
     }
