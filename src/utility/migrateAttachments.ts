4fb2ed9ad6272aa189261140265bbc0620aabd90
alex <alex@targoninc.com>
2026-01-10 19:52:53 +0100

    log other stuff

--- src/utility/migrateAttachments.ts
+++ src/utility/migrateAttachments.ts
@@ -1,8 +1,9 @@
 import {readdir} from "node:fs/promises";
 import {env} from "./Environment.ts";
+import {MediaFileType} from "../models/enums/MediaFileType.ts";
 
 export async function migrateAttachments() {
-    const dir = env<string>("ARTIST_SPACE_STORAGE_PATH");
+    const dir = env<string>("ARTIST_SPACE_STORAGE_PATH") + `/tridrive-1/${MediaFileType.albumFile}`;
     const existingAttachments = await readdir(dir);
     console.log(`Found ${existingAttachments.length} attachments in storage.`);
 
