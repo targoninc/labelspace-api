9ac852458f72c5a037e337035100326ef2116fab
Alexander Fritsch <alex@targoninc.com>
2026-01-13 10:33:02 +0100

    don't allow setting artists on creation

--- src/endpoints/albums/actions/CreateAlbumAttachmentEndpoint.ts
+++ src/endpoints/albums/actions/CreateAlbumAttachmentEndpoint.ts
@@ -13,7 +13,7 @@ export class CreateAlbumAttachmentEndpoint extends AuthenticatedPostEndpoint {
     }
 
     async run(req: AuthenticatedRequest, res: Response) {
-        let {albumId, name, artists} = req.body;
+        let {albumId, name} = req.body;
         if (!albumId || !name) {
             return res.status(400).send({error: "Missing albumId"});
         }
@@ -27,24 +27,7 @@ export class CreateAlbumAttachmentEndpoint extends AuthenticatedPostEndpoint {
             return res.status(404).send({error: "Album not found"});
         }
 
-        artists ??= "";
-        const artistsList = artists.split(",").map((a: string) => a.trim());
-        const tracks = await this.db.getTracksByAlbumIds([albumId]);
-
-        for (const artist of artistsList) {
-            const dbArtist = await this.db.getArtistByName(artist);
-            if (!dbArtist) {
-                return res.status(404).send({error: `Artist not found: ${artist}`});
-            }
-
-            const isArtistOnAlbum = album.artists.split(",").map((a: string) => a.trim()).includes(artist);
-            const isArtistOnTracks = tracks.some(t => t.artists.split(",").map((a: string) => a.trim()).includes(artist));
-            if (!isArtistOnAlbum && !isArtistOnTracks) {
-                return res.status(403).send({error: `Artist ${artist} is not associated with the album or its tracks`});
-            }
-        }
-
-        const attachmentId = await this.db.createAlbumAttachment(albumId, name, artists);
+        const attachmentId = await this.db.createAlbumAttachment(albumId, name, "");
         return res.send({attachmentId});
     }
 }
