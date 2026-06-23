4fe255282d3508c000ee357e9a1dabb6c8a51ade
alex <alex@targoninc.com>
2026-06-24 00:00:15 +0200

    more design, make label name configurable

--- src/endpoints/config/GetPublicUiConfigEndpoint.ts
+++ src/endpoints/config/GetPublicUiConfigEndpoint.ts
@@ -1,6 +1,6 @@
 import {Application, Request, Response} from "express";
 import {GetEndpoint} from "../base/GetEndpoint.js";
-import {COMPANY_CONTACT, LABEL_UI_URL, PORTAL_API_URL} from "../../utility/Constants.ts";
+import {COMPANY_CONTACT, LABEL_NAME, LABEL_UI_URL, PORTAL_API_URL} from "../../utility/Constants.ts";
 
 export class GetPublicUiConfigEndpoint extends GetEndpoint {
     constructor(app: Application, path: string) {
@@ -12,6 +12,7 @@ export class GetPublicUiConfigEndpoint extends GetEndpoint {
             labelUiUrl: LABEL_UI_URL,
             portalApiUrl: PORTAL_API_URL,
             contactEmail: COMPANY_CONTACT,
+            labelName: LABEL_NAME,
         });
     }
 }
